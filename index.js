8ff04242d8dc0f1987d45562fa726554da938356
Erik Vold <erikvvold@gmail.com>
2013-06-25 20:22:49 -0700

    no longer using raw.github.com

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+	var out = document.getElementById('output');
+	out.setAttribute('style', 'border: red solid;');
+	InkBlot({
+		context: out.getContext('2d'),
+		height: out.height,
+		width: out.width/2,
+		amount: 50000,
+		onComplete: function() {
+			out.setAttribute('style', 'border: green solid;');
+		}
+	});
